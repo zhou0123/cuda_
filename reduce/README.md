3b6a1588bcbb2e91f1083855739296af52b7d94f
zhou0123 <zhou812986561@gmail.com>
2022-09-06 18:58:45 +0800

    1

---
+++ reduce/README.md
@@ -0,0 +1,6 @@
+reduce_v0 :time 2.94ms
+reduce_v1 :time 2.13ms 解决线程分歧
+reduce_v2 :time 2.05ms 解决bank conflict
+reduce_v3 :time 1.08ms 末尾展开，warp
+reduce_v4 :time 1.03ms 完全展开
+reduce_v5 :time 0.548ms 解决idle线程
---
+++ reduce/reduce_v1.cu
@@ -0,0 +1,78 @@
+#include<bits/stdc++.h>
+#include<cuda.h>
+#include"cuda_runtime.h"
+#include"device_launch_parameters.h"
+#include<time.h>
+#include<sys/time.h>
+#define THREAD_PER_BLOCK 256
+
+bool check(float *a,float *b,int block_num)
+{
+    for (int i=0;i<block_num;i++)
+    {
+        if (a[i]!=b[i]) 
+        {
+            return false;
+        }
+    }
+    return true;
+}
+__global__ void reduce(float* in ,float * out)
+{
+    __shared__ float shared_memory[THREAD_PER_BLOCK];
+
+    unsigned int tid = threadIdx.x;
+    unsigned int id = blockIdx.x*blockDim.x+threadIdx.x;
+    shared_memory[tid] = in[id];
+    __syncthreads();
+
+    for (unsigned int i=1;i<blockDim.x;i*=2)
+    {
+        int index = i*2*tid;
+        //解决线程分歧
+        if (index<blockDim.x)
+        {
+            shared_memory[index] += shared_memory[index+i];
+        }
+        __syncthreads();
+    }
+
+    if (tid==0) out[blockIdx.x] = shared_memory[0];
+}
+int main()
+{
+    const int N  = 32*1024*1024;
+    float * a = (float*)malloc(N*sizeof(float));
+    float * d_a ;
+    cudaMalloc((void**)&d_a,N*sizeof(float));
+
+    const int block_num = N/THREAD_PER_BLOCK;
+
+    float * out = (float*)malloc(block_num*sizeof(float));
+    float * d_out;
+    cudaMalloc((void**)&d_out,block_num*sizeof(float));
+    float *res  = (float*)malloc(block_num*sizeof(float));
+    for (int i=0;i<N;i++) a[i]=1;
+    for (int i=0;i<block_num;i++)
+    {
+        int cur = 0;
+        for (int j=0;j<THREAD_PER_BLOCK;j++)
+        {
+            cur += a[i*THREAD_PER_BLOCK+j];
+        }
+        res[i] = cur;
+    }
+    cudaMemcpy(d_a,a,N*sizeof(float),cudaMemcpyHostToDevice);
+    dim3 Grid (N/THREAD_PER_BLOCK,1);
+    dim3 Block (THREAD_PER_BLOCK,1);
+    reduce<<<Grid,Block>>>(d_a,d_out);
+    cudaMemcpy(out,d_out,block_num*sizeof(float),cudaMemcpyDeviceToHost);
+
+    if (check(out,res,block_num))
+    {
+        printf("true");  
+    }
+    else printf("wrong");
+    cudaFree(d_out);
+    cudaFree(d_a);
+}
